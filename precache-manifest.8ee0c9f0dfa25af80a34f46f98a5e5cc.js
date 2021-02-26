106ceca9649ca26958ffc66cf5defb1e14de4c5b
George <ghdiep@gmail.com>
2021-02-25 20:37:04 -0500

    Updates

---
+++ precache-manifest.8ee0c9f0dfa25af80a34f46f98a5e5cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c71080374336ce70db5",
+    "url": "/bee-recipes/static/css/main.ef9381b3.chunk.css"
+  },
+  {
+    "revision": "3c71080374336ce70db5",
+    "url": "/bee-recipes/static/js/main.3c710803.chunk.js"
+  },
+  {
+    "revision": "d8f749834689d1e2a1c4",
+    "url": "/bee-recipes/static/js/1.d8f74983.chunk.js"
+  },
+  {
+    "revision": "d17a6dc5162d94e742e8",
+    "url": "/bee-recipes/static/js/runtime~main.d17a6dc5.js"
+  },
+  {
+    "revision": "e3ba9a6b280e4971e76c09750bd61b8e",
+    "url": "/bee-recipes/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/bee-recipes/precache-manifest.8ee0c9f0dfa25af80a34f46f98a5e5cc.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/bee-recipes/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
