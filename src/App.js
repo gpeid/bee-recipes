1a9281dedf38b2d509c90cb68c11283d0d14f7b1
George <ghdiep@gmail.com>
2021-04-02 02:41:16 -0400

    add hashRouter

--- src/App.js
+++ src/App.js
@@ -5,24 +5,23 @@ import Header from './components/Header/Header';
 import RecipeContainer from './containers/RecipeContainer';
 import Card from './components/Card/Card';
 // import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { HashRouter, Route, Switch } from "react-router-dom";
-
-import Recipe from './pages/recipe/recipe';
+import { HashRouter, Route, Switch } from 'react-router-dom';
+import RecipePage from './pages/RecipePage';
 
 const App = () => {
-  return (
-    <HashRouter basename="/">
-      <div className={styles.App}>
-        <Header />
-        <Route exact path="/" component={RecipeContainer} />
-        <Route path="/random" component={Card} />
-        <Switch>
-          <Route path="/recipe/:id" render={(props) => <Recipe {...props} />} />
-        </Switch>
-        <Footer />
-      </div>
-    </HashRouter>
-  );
+	return (
+		<HashRouter basename="/">
+			<div className={styles.App}>
+				<Header />
+				<Route exact path="/" component={RecipeContainer} />
+				<Route path="/random" component={Card} />
+				<Switch>
+					<Route path="/recipe/:id" render={RecipePage} />
+				</Switch>
+				<Footer />
+			</div>
+		</HashRouter>
+	);
 };
 
 export default App;
