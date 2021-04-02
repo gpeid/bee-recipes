c5c8321cd8ed4d764b1fde5bdea5060b74a6a4ae
George <ghdiep@gmail.com>
2021-04-02 00:35:59 -0400

    change to functional component, use HashRouter

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
 import styles from './App.module.css';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import RecipeContainer from './containers/RecipeContainer';
 import Card from './components/Card/Card';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter, Route, Switch } from "react-router-dom";
+
 import Recipe from './pages/recipe/recipe';
 
-class App extends Component {
-	render() {
-		return (
-			<Router>
-				<div className={styles.App}>
-					<Header />
-					<Route exact path="/" component={RecipeContainer} />
-					<Route path="/random" component={Card} />
-					<Switch>
-						<Route path="/recipe/:id" render={(props) => <Recipe {...props} />} />
-					</Switch>
-					<Footer />
-				</div>
-			</Router>
-		);
-	}
-}
+const App = () => {
+  return (
+    <HashRouter basename="/">
+      <div className={styles.App}>
+        <Header />
+        <Route exact path="/" component={RecipeContainer} />
+        <Route path="/random" component={Card} />
+        <Switch>
+          <Route path="/recipe/:id" render={(props) => <Recipe {...props} />} />
+        </Switch>
+        <Footer />
+      </div>
+    </HashRouter>
+  );
+};
 
 export default App;
