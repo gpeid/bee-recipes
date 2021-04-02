d90995032039474d8536ff74af45a1ab9d68101d
George <ghdiep@gmail.com>
2021-04-02 00:16:02 -0400

    use material button

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,14 +1,15 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
+import { Button } from '@material-ui/core';
 
 export function RecipeCounter() {
-  const [count, setCount] = useState(0);
-  console.log(count);
-  return (
-    <div>
-      <p>{count} people have used this recipe</p>
-      <button onClick={() => setCount(count + 1)}>
-        I've used this recipe!
-      </button>
-    </div>
-  );
+	const [count, setCount] = useState(0);
+	console.log(count);
+	return (
+		<div>
+			<p>{count} people have used this recipe</p>
+			<Button variant="contained" color="primary" onClick={() => setCount(count + 1)}>
+				I've used this recipe!
+			</Button>
+		</div>
+	);
 }
