699c64771e4ec67a5e6444fd84f6bb38e41703b9
George <ghdiep@gmail.com>
2021-04-02 00:14:26 -0400

    add routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React, { Component } from "react";
-import styles from "./App.module.css";
-import Footer from "./components/Footer/Footer";
-import Header from "./components/Header/Header";
-import RecipeContainer from "./containers/RecipeContainer";
-import Recipe from "./components/Recipe/Recipe";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import React, { Component } from 'react';
+import styles from './App.module.css';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import RecipeContainer from './containers/RecipeContainer';
+import Card from './components/Card/Card';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Recipe from './pages/recipe/recipe';
 
 class App extends Component {
-  render() {
-    return (
-      <Router>
-        <div className={styles.App}>
-          <Header />
-          <Route exact path="/" component={RecipeContainer} />
-          <Route exact path="/random" component={Recipe} />
-          <Footer />
-        </div>
-      </Router>
-    );
-  }
+	render() {
+		return (
+			<Router>
+				<div className={styles.App}>
+					<Header />
+					<Route exact path="/" component={RecipeContainer} />
+					<Route path="/random" component={Card} />
+					<Switch>
+						<Route path="/recipe/:id" render={(props) => <Recipe {...props} />} />
+					</Switch>
+					<Footer />
+				</div>
+			</Router>
+		);
+	}
 }
 
 export default App;
