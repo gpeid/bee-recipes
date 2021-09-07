7769fa48f2fc60ab3fa97fe582c38cea5ba78841
George Diep <ghdiep@gmail.com>
2021-09-06 22:42:24 -0400

    not sure

--- src/pages/RecipePage.js
+++ src/pages/RecipePage.js
@@ -2,10 +2,8 @@ import React from 'react';
 import RecipeDetails from './RecipeDetails/RecipeDetails';
 
 const RecipePage = (props) => {
-  
-  console.log(props);
   return (
-      <RecipeDetails props={props}/>
+      <RecipeDetails details={props}/>
   );
 };
 
