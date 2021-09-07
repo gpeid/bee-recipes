53a8f58f53c7ffa197ea9464c3582ce619e3cc95
George Diep <ghdiep@gmail.com>
2021-09-06 22:41:30 -0400

    update details component

--- src/pages/RecipeDetails/RecipeDetails.js
+++ src/pages/RecipeDetails/RecipeDetails.js
@@ -1,11 +1,38 @@
-import React from 'react';
-import { withRouter } from 'react-router-dom';
+import React, { useEffect } from "react";
+import { withRouter } from "react-router-dom";
 
-const RecipeDetails = (props) => {
-	return (
-		<div>
-			<h1>{props.location.state.name}</h1>
-		</div>
-	);
+const RecipeDetails = ({ location, history }) => {
+  console.log(location);
+  useEffect(() => {
+    !location.state && history.goBack();
+  }, []);
+
+  try {
+    const listIngredients = location.state.ingredients ? (
+      <ul>
+        {location.state.ingredients.map((ingredient, i) => {
+          return <li>{ingredient}</li>;
+        })}
+      </ul>
+    ) : null;
+    const displayDetails = (
+      <div>
+        <h1>{location.state.name}</h1>
+        <p>{location.state.diet_type}</p>
+        {listIngredients}
+        <p>{location.state.notes}</p>
+        <button
+          onClick={() => {
+            history.goBack();
+          }}
+        >
+          Go back
+        </button>
+      </div>
+    );
+    return displayDetails;
+  } catch (err) {
+    return "There was an error";
+  }
 };
 export default withRouter(RecipeDetails);
